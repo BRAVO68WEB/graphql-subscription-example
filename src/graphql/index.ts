d5b4cadbcb381a9be1c24a1ae47eae28ae0e5cbb
bravo68web <jbandyopadhayaya@gmail.com>
2024-09-07 09:11:28 +0000

    chore: added example code

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,5 +1,6 @@
 import { createYoga } from "graphql-yoga";
 import { Context, pubSub, schema } from "../schema";
+// import { useJWT, extractFromHeader, createInlineSigningKeyProvider } from "@graphql-yoga/plugin-jwt";
 
 export const yoga = createYoga<Context>({
     schema,
@@ -8,4 +9,26 @@ export const yoga = createYoga<Context>({
     graphiql: {
         subscriptionsProtocol: 'WS', // use WebSockets instead of SSE
     },
+    plugins: [
+        // useJWT({
+        //     singingKeyProviders: [
+        //         createInlineSigningKeyProvider('secret'),
+        //     ],
+        //     extendContext: true,
+        //     tokenLookupLocations: [
+        //         extractFromHeader({
+        //             name: 'authorization',
+        //             prefix: 'Bearer',
+        //         }),
+        //         extractFromHeader({ 
+        //             name: 'x-api-key', 
+        //             prefix: 'API-Access' 
+        //         }),
+        //     ],
+        //     reject: {
+        //         invalidToken: true,
+        //         missingToken: true,
+        //     }
+        // })
+    ]
 });
